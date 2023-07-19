ee8066833cb7d5421d80b0bca11c5db2f9e8b75f
Vedant-Sharda <66670776+Vedant-Sharda@users.noreply.github.com>
2023-07-19 13:27:32 +0530

    Just confirming minor changes and version.

--- Audio_Recorder.py
+++ Audio_Recorder.py
@@ -1,27 +1,24 @@
 import sounddevice as sd
 import soundfile as sf
 
-"""This file records audio for the specified duration upto 30 seconds and saves it as a wav file
+"""This file records audio for the specified duration and saves it as a wav file
 Returns 0 if successful else -1"""
-
-
 def record_audio(duration, filename):
     try:
         sample_rate = 44100
         channels = 1
-
         frames = int(duration * sample_rate)
 
         recording = sd.rec(frames, samplerate=sample_rate, channels=channels)
-
         sd.wait()
 
         # We save the recorded audio to a WAV file
         sf.write(filename, recording, sample_rate)
-        # This could help with error handling during the program
+
         return 0
-    except:
+    except sd.PortAudioError as e:
+        print("PortAudioError:", e)
         return -1
-
-
-
+    except Exception as e:
+        print("Error:", e)
+        return -1
